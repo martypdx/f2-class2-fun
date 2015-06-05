0246ce06558e158ab34d4f8e97e85bad1aaf60a9
marty <marty@martypdx.com>
2015-06-04 20:13:58 -0700

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  var srcFiles = ["lib/*.js"];
+
+  grunt.initConfig({
+    jshint: {
+      files: srcFiles,
+      options: {
+        sub: true
+      }
+    },
+    jscs: {
+      src: srcFiles,
+      options: {
+        // preset: "jquery",
+        requireDotNotation: null,
+        disallowMultipleVarDecl: null,
+        requireMultipleVarDecl: null
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-jscs");
+
+  grunt.registerTask("default", [ "jshint", "jscs" ]);
+
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+var getHello = require( './lib/getHello' );
+
+console.log( getHello('world') );
+
+console.log ( require('./lib/banana') );
